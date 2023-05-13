24b9e99aa23959b1758703ccbde3fde0c9dbd4c5
sepehr zamani <szamani3045@gmail.com>
2023-05-12 19:15:32 -0700

    update swiper

--- src/components/BestFinancialSolution/index.jsx
+++ src/components/BestFinancialSolution/index.jsx
@@ -1,7 +1,32 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import { TwoPartLayoutByImage } from "../";
-import data from "../../data/dummy";
+import data, { AngleLeftIcon, AngleRightIcon } from "../../data/dummy";
 import Item from "./Item";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const NextButton = () => {
+  const swiper = useSwiper();
+
+  return (
+    <button
+      className="text-custom-gray border border-custom-gray rounded-full p-2 w-[40px] h-[40px] hover:bg-custom-gray hover:text-white duration-150"
+      onClick={() => swiper.slideNext()}
+    >
+      <FontAwesomeIcon icon={AngleRightIcon} />
+    </button>
+  );
+};
+const PrevButton = () => {
+  const swiper = useSwiper();
+  return (
+    <button
+      className="text-custom-gray border border-custom-gray rounded-full p-2 w-[40px] h-[40px] hover:bg-custom-gray hover:text-white duration-150"
+      onClick={() => swiper.slidePrev()}
+    >
+      <FontAwesomeIcon icon={AngleLeftIcon} size="1x" fontWeight={"100"} />
+    </button>
+  );
+};
 
 const BestFinancialSolution = () => {
   const { bestFinancialSolution } = data;
@@ -21,6 +46,10 @@ const BestFinancialSolution = () => {
               </SwiperSlide>
             );
           })}
+          <div className="space-x-2 z-30 absolute bottom-0 left-0 container w-full hidden lg:block">
+            <PrevButton />
+            <NextButton />
+          </div>
         </Swiper>
       </div>
     </TwoPartLayoutByImage>
